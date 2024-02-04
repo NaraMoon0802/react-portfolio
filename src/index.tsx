f66fedc60ebdb6164603fc5253c38b6a6cea8cc1
NaraMoon0802 <75877923+NaraMoon0802@users.noreply.github.com>
2024-02-04 13:57:21 +0900

    追加修正

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {RollText} from './rollText';
+import {RollText} from './component/rollText';
 import {ImagePortfolioList} from './imagePortfolioList';
+import ScrollNotice from './component/scrollNotice';
 import './scss/index.scss';
 
 const App = () => {
@@ -12,16 +13,16 @@ const App = () => {
             <nav className="l-nav">
                 <li className="heading hover" onClick={() => {setIsHome(true)}}>Home</li>
                 <li className="heading hover is_active" onClick={() => {setIsHome(false)}}>Gallery</li>
-                <li className="heading hover" onClick={() => {setIsHome(true)}}>Profile</li>
-                <li className="heading hover" onClick={() => {setIsHome(true)}}>Contact</li>
+                <li className="heading hover" onClick={() => {setIsHome(true)}}>Login</li>
             </nav>
             <ImagePortfolioList/>
+            {!isHome && <ScrollNotice/>}
             <main className={`l-main -home ${isHome ? 'fadein' : 'fadeout'}`}>
                 <div className="c-logo">
                     <div className="__title large_heading">
                         Welcome to Portfolio Gallery
                     </div>
-                    <RollText setView={() => {setIsHome(false)}}/>
+                    <RollText sizePx={200} text={'OPEN OPEN OPEN OPEN OPEN'} setView={() => {setIsHome(false)}}/>
                 </div>
             </main>
         </>
